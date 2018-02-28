6d4834733f9c4639e9fda45ea0e4bb497957aac7
Tigbemileke Ojo <gbemiojo@Tigbemilekes-MacBook-Pro.local>
2018-02-28 08:49:55 -0500

    Refactored DOM queries to jQuery and added a slider function that is currently not working

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var body = document.querySelector('body');
-var galleryList = document.querySelector('.gallery__list');
-var lightbox = document.querySelector('.lightbox');
-var lightboxLayout = document.querySelector('.lightbox__layout');
-var lightboxClose = document.querySelector('.lightbox__close');
+var $body = $('body')[0];
+var $galleryList = $('.gallery__list')[0];
+var $highlight = $('.highlight')[0];
+var $highlightLayout = $('.highlight__layout')[0];
+var $highlightClose = $('.highlight__close')[0];
 
 var images = [
   {
@@ -28,6 +28,22 @@ var images = [
   {
     src: 'https://images.pexels.com/photos/327502/pexels-photo-327502.jpeg?w=940&h=650&dpr=2&auto=compress&cs=tinysrgb',
     alt: 'City 6'
+  },
+  {
+    src: 'https://images.pexels.com/photos/374827/pexels-photo-374827.jpeg?w=940&h=650&dpr=2&auto=compress&cs=tinysrgb',
+    alt: 'City 7'
+  },
+  {
+    src: 'https://images.pexels.com/photos/802024/pexels-photo-802024.jpeg?w=940&h=650&dpr=2&auto=compress&cs=tinysrgb',
+    alt: 'City 8'
+  },
+  {
+    src: 'https://images.pexels.com/photos/757432/pexels-photo-757432.jpeg?w=940&h=650&dpr=2&auto=compress&cs=tinysrgb',
+    alt: 'City 9'
+  },
+  {
+    src: 'https://images.pexels.com/photos/358243/pexels-photo-358243.jpeg?w=940&h=650&dpr=2&auto=compress&cs=tinysrgb',
+    alt: 'City 10'
   }
 ];
 
@@ -39,14 +55,17 @@ var createImg = function(obj) {
   return img;
 }
 
-var turnLightbox = function(str) {
+var turnHighlight = function(str) {
   if (str === 'on') {
-    body.classList.add('lightbox-is-active');
+    $body.classList.add('highlight-active');
   } else {
-    body.classList.remove('lightbox-is-active');
+    $body.classList.remove('highlight-active');
   }
 }
 
+
+//creating the gallery list item in the dom. 
+//adding images to each list item.
 var createGalleryItem = function(obj) {
   var li = document.createElement('li');
   li.classList.add('gallery__item');
@@ -58,17 +77,17 @@ var createGalleryItem = function(obj) {
   return li;
 }
 
-var lightboxImg = createImg(images[0]);
-lightboxImg.classList.add('lightbox__img');
-lightboxLayout.appendChild(lightboxImg);
+var highlightImg = createImg(images[0]);
+highlightImg.classList.add('highlight__img');
+$highlightLayout.appendChild(highlightImg);
 
 var armImg = function(node) {
   node.addEventListener('click', function(e) {
     var elem = e.target;
     var src = elem.src;
-    lightboxImg.src = src;
+    highlightImg.src = src;
 
-    turnLightbox('on');
+    turnHighlight('on');
   });
 }
 
@@ -77,18 +96,18 @@ var galleryItems = images.map(function(obj) {
 });
 
 galleryItems.forEach(function(node) {
-  galleryList.appendChild(node);
+  $galleryList.appendChild(node);
   armImg(node);
 });
 
-var closeDivs = [lightboxClose,lightboxLayout];
+var closeDivs = [$highlightClose,$highlightLayout];
 
 closeDivs.forEach(function(node) {
   node.addEventListener('click', function(e) {
     e.preventDefault();
   
     if(e.target === node) {
-      turnLightbox('off');
+      turnHighlight('off');
     }
   });
 });
@@ -97,6 +116,33 @@ window.addEventListener('keyup', function(e) {
   e.preventDefault();
 
   if(e.code === 'Escape') {
-    turnLightbox('off');
+    turnHighlight('off');
   }
+});
+
+//Doesn't do anything as of yet
+$(function() {
+    
+    var animationSpeed = 1000;
+    var pause = 3000;
+    var currentSlide = 1;
+
+    //cache the DOM
+    var $slider = $('.highlight_img' );
+    var $slideContainer = $('.highlight_layout');
+
+  setInterval(function() {  
+    $slideContainer.animate({'margin-left': '-=90%'}, animationSpeed, function() {
+      currentSlide++;
+      if (currentSlide === $slides.length); {
+        currentSlide = 1;
+        $slideContainer.css('margin-left', 0);
+      }
+    });
+  }, pause);
+  //setInterval
+    //animate margin-left
+    //if it's last slide, go to position 1 (0px);
+  //listen for mouseenter and pause
+  //resume on mouseleave
 });
